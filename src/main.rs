e907b6afe3ea1c387065b87c6b514479690f451e
Andrés Testi <andres.a.testi.-nd@disney.com>
2019-05-18 11:34:52 -0300

    Plot the last year evolution

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,47 @@
-use std::error::Error;
-use chrono::NaiveDate;
+use chrono::{NaiveDate, Utc};
+use gnuplot::*;
 use serde::Deserialize;
-use tensorflow::Tensor;
 
-static DOLLAR_URL: &str = "http://infra.datos.gob.ar/catalog/sspm/dataset/168/distribution/168.1/download/datos-tipo-cambio-usd-futuro-dolar-frecuencia-diaria.csv";
+const DOLLAR_URL: &str = "http://infra.datos.gob.ar/catalog/sspm/dataset/168/distribution/168.1/\
+                          download/datos-tipo-cambio-usd-futuro-dolar-frecuencia-diaria.csv";
 
 #[derive(Debug, Deserialize)]
 struct Record {
     indice_tiempo: NaiveDate,
     tipo_cambio_bna_vendedor: Option<f32>,
     tipo_cambio_a3500: Option<f32>,
-    tipo_cambio_mae: Option<f32>,
-    volumen_mae: Option<f32>,
-    tipo_cambio_implicito_en_adrs: Option<f32>,
-    futuro_rofex_usd1m: Option<f32>,
-    interes_abierto_1m: Option<f32>,
-    futuro_rofex_usd2m: Option<f32>,
-    interes_abierto_2m: Option<f32>,
-    futuro_rofex_usd3m: Option<f32>,
-    interes_abierto_3m: Option<f32>,
-    futuro_rofex_usd4m: Option<f32>,
-    interes_abierto_4m: Option<f32>,
-    futuro_rofex_usd5m: Option<f32>,
-    interes_abierto_5m: Option<f32>,
-    futuro_rofex_usd6m: Option<f32>,
-    interes_abierto_6m: Option<f32>,
-    futuro_rofex_usd_12m: Option<f32>,
-    interes_abierto_12m: Option<f32>,
 }
 
-/// split a univariate sequence into samples
-fn split_sequence(sequence: Vec<f32>, n_steps: usize) -> (Vec<Vec<f32>>, Vec<f32>){
-    let mut x = Vec::new();
-    let mut y = Vec::new();
-
-    for i in 0..sequence.len() {
-        // find the end of this pattern
-        let end_ix = i + n_steps;
-
-        // check if we are beyond the sequence
-        if end_ix > sequence.len() -1 {
-            break;
-        }
-
-        // gather input and output parts of the pattern
-        let seq_x = sequence[i..end_ix].to_vec();
-        let seq_y = sequence[end_ix];
-        x.push(seq_x);
-        y.push(seq_y);
-    }
-    (x, y)
-}
-
-fn download_usd() -> Vec<f32> {
-
-    let client = reqwest::Client::new();
-    let res = client.get(DOLLAR_URL).send().expect("Unable to download csv");
-
+fn download_usd() -> (Vec<i64>, Vec<f32>) {
+    let res = reqwest::Client::new()
+        .get(DOLLAR_URL)
+        .send()
+        .expect("Unable to download csv");
+    let past = Utc::today().naive_utc() - chrono::Duration::days(365);
     csv::Reader::from_reader(res)
         .deserialize::<Record>()
-        .map(|r|
-            r
-                .expect("Unable to deserialize record")
-                .tipo_cambio_a3500
-                .expect("Field not found"))
-        .collect()
+        .filter_map(Result::ok)
+        .filter(|r| r.indice_tiempo > past)
+        .filter_map(|r| {
+            r.tipo_cambio_a3500
+                .map(|p| (r.indice_tiempo.and_hms(0, 0, 0).timestamp(), p))
+        })
+        .unzip()
 }
 
 fn main() {
-    // choose a number of time steps
-    let n_steps = 4;
+    let (xs, ys) = download_usd();
 
-    let mut historic_data = download_usd();
-    let len = historic_data.len() - 20;
-    historic_data.split_off(len);
-
-    // split into samples
-    let (x, y) = split_sequence(historic_data, n_steps);
-    println!("secuencias x {:?}", x);
+    let mut fg = Figure::new();
+    fg.axes2d()
+        .set_title("USD/ARS - US Dollar Argentinian Peso", &[])
+        .set_legend(Graph(0.5), Graph(0.9), &[], &[])
+        .set_x_label("Date", &[])
+        .set_x_grid(true)
+        .set_x_time(true)
+        .set_x_ticks(Some((Auto, 1)), &[Format("%b %Y")], &[Rotate(45.0)])
+        .set_y_label("Price", &[])
+        .set_y_grid(true)
+        .lines(&xs, &ys, &[Caption("USD/ARS")]);
+    fg.show();
 }
